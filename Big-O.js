3e65822074fa4a3ad07feffd4a09a877e620e3b3
Tatineia Duarte <tatineiaduarte@MacBook-Pro-de-Tatineia.local>
2022-09-28 06:34:20 -0400

    Test Repository

---
+++ Big-O.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * A notação Big O não fornece o tempo em segundos. A notação Big O,
+ * permite que você compare o número de operações. Ela informa o quão rápido um algoritmo cresce.
+ * 
+ * A notação do Big O leva em consideração a pior das hipoteses. 
+ * 
+ * A rapidez de um algoritmo não é medida em segundos, mas pelo crescimento do número de operações.
+ * O(log n) é mais rápido que O(n), e ele fica mais rápido conforme a lista aumenta.
+ * 
+ * 
+ * 
+ * Exercícios:
+ * Forneça o tempo de execução para cada um dos casos a seguir em termos da notação Big O
+ * 
+ * 1.3 Você tem um nome e deseja encontrar o número de telefone para esse nome em uma agenda telefônica.
+ * R: O (log n)
+ * 
+ * 1.4 Você tem um número de telefone e desenha encontrar o dono dele em uma agenda telefônica. ( Dica: deve procurar pela agenda inteira!)
+ * R: O(n)
+ * 
+ * 1.5 Você quer ler os número de cada pessoa da agenda telefônica
+ * R: O(n)
+ * 
+ * 1.6 Você quer ler o números apenas dos nomes que começam com A. 
+ * R: O(n) -> Independente se quero só ler com a letra A 
+ * 
+ * 
+ */
