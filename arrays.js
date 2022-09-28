f716602fa89f6a8a222aac37a528b4a668c890e5
Tatineia Duarte <tatineiaduarte@MacBook-Pro-de-Tatineia.local>
2022-09-28 07:36:07 -0400

    Add new array-script

---
+++ arrays.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * Arrays e Listas
+ * 
+ * Tempo de execução para arrays e Listas
+ * 
+ * Array
+ * leitura: O(1)
+ * Inserção: O(n)
+ * 
+ * Lista
+ * leitura: O(n)
+ * inserção: O(1)
+ * 
+ */
+
+//Exercicios
+/**
+ * 
+ * Suponha que você esteja criando um aplicativo para acompanharas suas finanças.
+ * 
+ * Todos os dias você anotará tudo que gastou e onde gastou. No final do mês, você deverá revisar os seus gastos e resumir o quanto
+ * gastou. Logo, você terá um monte de inserções e poucas leituras. Você deverá usar um array ou uma lista para implementar este aplicativo?
+ * 
+ * 
+ * R: A melhor alternativa é a lista encadeada, dado que haverá mais insert do que leitura e a lista tem um tempo de exeução mais rápido para
+ * insert.
+ * 
+ */
